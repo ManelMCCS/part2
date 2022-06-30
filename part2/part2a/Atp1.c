5ab66abd4df54c6c8d4e556d8d5c164b75dffb15
ManelMCCS <up201806391@fe.up.pt>
2022-06-30 17:50:47 +0100

    exe1 done

--- part2/part2a/Atp1.c
+++ part2/part2a/Atp1.c
@@ -2,44 +2,48 @@
 #include <unistd.h>
 #include <time.h>
 
-#define HOW_MANY_FORKS 2 //Number of forks. Testing from 1 to 4 should be enough
-//#define ENABLE_HEAVY_LOAD
+#define HOW_MANY_FORKS 2 // Number of forks. Testing from 1 to 4 should be enough
+#define ENABLE_HEAVY_LOAD
 
-unsigned long _timestamp_sec(clockid_t clk_id){
+unsigned long _timestamp_sec(clockid_t clk_id)
+{
     struct timespec tp;
     clock_gettime(clk_id, &tp);
     return tp.tv_sec;
 }
 
-void heavy_load() {
-    unsigned long i = 10000000000L;
-    while (i--);
+void heavy_load()
+{
+    unsigned long my_cpu_wait_15s = 4500000000L;
+    while (my_cpu_wait_15s)
+    {
+        my_cpu_wait_15s--;
+    }
 }
 
-int main() {
-  
+int main()
+{
+
     printf("Starting with process id: %d\n", getpid());
 
-    for (int i=0; i<HOW_MANY_FORKS; i++) 
+    for (int i = 0; i < HOW_MANY_FORKS; i++)
         fork();
-  
+
     printf("Running process id: %d\n", getpid());
 
 #ifdef ENABLE_HEAVY_LOAD
-    {
-        unsigned long process_ts_start = _timestamp_sec(CLOCK_PROCESS_CPUTIME_ID);
-        unsigned long system_ts_start = _timestamp_sec(CLOCK_REALTIME);
 
-        heavy_load(); 
+    unsigned long process_ts_start = _timestamp_sec(CLOCK_PROCESS_CPUTIME_ID);
+    unsigned long system_ts_start = _timestamp_sec(CLOCK_REALTIME);
 
-        printf("Done with pid: %d in (%lu process seconds) and (%lu system seconds).\n", 
-            getpid(), 
-            _timestamp_sec(CLOCK_PROCESS_CPUTIME_ID)-process_ts_start,
-            _timestamp_sec(CLOCK_REALTIME)-system_ts_start
-        );
-    }
+    heavy_load();
+
+    printf("Done with pid: %d in (%lu process seconds) and (%lu system seconds).\n",
+           getpid(),
+           _timestamp_sec(CLOCK_PROCESS_CPUTIME_ID) - process_ts_start,
+           _timestamp_sec(CLOCK_REALTIME) - system_ts_start);
+
 #endif
 
     return 0;
 }
-
